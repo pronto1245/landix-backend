c372469662b8a91bfd65db03b8782c377cb36890
Evgen4ikk <8evgentys8@mail.ru>
2025-09-16 15:46:55 +0700

    fix: исправил ссылку для доки

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   app.use('/assets', express.static(join(process.cwd(), '/templates/assets')));
 
