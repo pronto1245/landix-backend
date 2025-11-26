4be1e9fb285e26b3a11ba301492d5fdf7f78863f
Evgen4ikk <8evgentys8@mail.ru>
2025-11-26 16:43:40 +0700

    исправил dto

--- src/flows/dto/update-landing.dto.ts
+++ src/flows/dto/update-landing.dto.ts
@@ -1,10 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateLandingDto {
   @ApiProperty({
-    type: String,
     description: 'ID лендинга',
-    example: 'abce1234-5678-90ff-1122-334455'
+    example: 'c0d8f0ae-7238-4419-9218-b0b586c91387'
   })
+  @IsString()
+  @IsNotEmpty()
   landingId: string;
 }
