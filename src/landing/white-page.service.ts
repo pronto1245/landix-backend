abd241310d5d33984169e3c448eb842b6a6a9391
Evgen4ikk <8evgentys8@mail.ru>
2025-12-11 18:29:48 +0700

    исправил отображени домена

--- src/landing/white-page.service.ts
+++ src/landing/white-page.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Request } from 'express';
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
@@ -8,20 +7,12 @@ export class WhitePageService {
   private template: string;
 
   constructor() {
-    // Абсолютный путь до templates/whitePages/default.html
     const templatePath = join(process.cwd(), 'templates/whitePages/page1.html');
 
     this.template = readFileSync(templatePath, 'utf8');
   }
 
-  getDomain(req: Request): string {
-    const host = (req.headers['x-forwarded-host'] as string | undefined) ?? req.headers.host ?? '';
-
-    return host.split(':')[0].toLowerCase();
-  }
-
-  render(req: Request): string {
-    const domain = this.getDomain(req);
+  render(domain: string): string {
     return this.template.replaceAll('{DOMAIN}', domain);
   }
 }
